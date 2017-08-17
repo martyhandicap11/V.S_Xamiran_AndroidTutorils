112011d477699d53d6d9c09b1ee1091c0ec63571
martyhandicap11 <mcooney17.com>
2017-08-17 13:34:20 +0100

    Creating a Simple Listview

---
+++ listViewTutorial/listViewTutorial/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace listViewTutorial
+{
+    [Activity(Label = "listViewTutorial", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        //int count = 1;
+        private List<string> mItems;
+        private ListView mListView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            mListView = FindViewById<ListView>(Resource.Id.myListView);
+
+            mItems = new List<string>();
+            mItems.Add("Bob");
+            mItems.Add("Tom");
+            mItems.Add("Jim");
+
+            MyListViewAdapter adapter = new MyListViewAdapter(this, mItems);
+
+            string indexTest = adapter.mItems[1];
+
+
+
+            mListView.Adapter = adapter;
+
+
+        }
+    }
+}
+
