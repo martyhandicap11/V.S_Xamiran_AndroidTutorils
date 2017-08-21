b3fdb5f6cf6fe3e8a12503bcf2d92672e14f2953
martyhandicap11 <mcooney17.com>
2017-08-21 17:01:46 +0100

    Creating a Dialog Fragment

--- LoginSystem/LoginSystem/MainActivity.cs
+++ LoginSystem/LoginSystem/MainActivity.cs
@@ -1,18 +1,32 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace LoginSystem
 {
     [Activity(Label = "LoginSystem", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+
+        private Button mBtnSignUp;
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+
+            mBtnSignUp.Click += (object sender, EventArgs args) =>
+            {
+                //Pull up dilaog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                dialog_SignUp signUpDialog = new dialog_SignUp();
+                signUpDialog.Show(transaction, "dialog fragment");
+            };
+
         }
     }
 }
