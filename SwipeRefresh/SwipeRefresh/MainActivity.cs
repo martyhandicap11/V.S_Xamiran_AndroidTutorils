3faf7f3bf7c12bf2ccc6ec8a55baf29883bb9158
martyhandicap11 <mcooney17.com>
2017-08-29 12:41:46 +0100

    Swipe Refresh Layout

---
+++ SwipeRefresh/SwipeRefresh/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.Widget;
+using System.ComponentModel;
+using System.Threading;
+using Android;
+
+namespace SwipeRefresh
+{
+    [Activity(Label = "SwipeRefreshTutorial", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        SwipeRefreshLayout mSwipeRefreshLayout;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            mSwipeRefreshLayout = FindViewById<SwipeRefreshLayout>(Resource.Id.swipeLayout);
+#pragma warning disable CS0618 // Type or member is obsolete
+            mSwipeRefreshLayout.SetColorScheme(Android.Resource.Color.HoloBlueBright, Android.Resource.Color.HoloBlueDark, Android.Resource.Color.HoloGreenLight, Android.Resource.Color.HoloRedLight);
+#pragma warning restore CS0618 // Type or member is obsolete
+            mSwipeRefreshLayout.Refresh += mSwipeRefreshLayout_Refresh;
+        }
+
+        void mSwipeRefreshLayout_Refresh(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.RunWorkerAsync();
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            RunOnUiThread(() => { mSwipeRefreshLayout.Refreshing = false; });
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Will run on separate thread
+            Thread.Sleep(3000);
+        }
+
+    }//end of class
+}//end of namespace
+
